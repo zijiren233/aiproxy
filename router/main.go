f6fbb8b163e2532569daee425db8e2390571e0bd
zijiren233 <pyh1670605849@gmail.com>
2025-03-24 16:33:49 +0800

    feat: basic aiproxy http handler

--- router/main.go
+++ router/main.go
@@ -1,10 +1,15 @@
 package router
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func SetRouter(router *gin.Engine) {
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "AI Proxy is running!")
+	})
 	SetAPIRouter(router)
 	SetRelayRouter(router)
 	SetSwaggerRouter(router)
