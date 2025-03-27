2b73d782ff311fb1baebdcaecad1a01590e0188a
zijiren233 <pyh1670605849@gmail.com>
2025-03-27 14:22:54 +0800

    fix: ci lint

--- common/ipblack/main.go
+++ common/ipblack/main.go
@@ -5,19 +5,27 @@ import (
 	"time"
 
 	"github.com/labring/aiproxy/common"
+	log "github.com/sirupsen/logrus"
 )
 
 func SetIPBlack(ip string, duration time.Duration) {
 	if common.RedisEnabled {
-		redisSetIPBlack(context.Background(), ip, duration)
-	} else {
-		memSetIPBlack(ip, duration)
+		_, err := redisSetIPBlack(context.Background(), ip, duration)
+		if err == nil {
+			return
+		}
+		log.Errorf("failed to set IP %s black: %s", ip, err)
 	}
+	memSetIPBlack(ip, duration)
 }
 
 func GetIPIsBlock(ctx context.Context, ip string) (bool, error) {
 	if common.RedisEnabled {
-		return redisGetIPIsBlock(ctx, ip)
+		ok, err := redisGetIPIsBlock(ctx, ip)
+		if err == nil {
+			return ok, nil
+		}
+		log.Errorf("failed to get IP %s is block: %s", ip, err)
 	}
 	return memGetIPIsBlock(ip)
 }
