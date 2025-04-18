0c424b70f78db0e0e6a91bfe24f7980385ed4a5d
zijiren233 <pyh1670605849@gmail.com>
2025-04-19 00:46:59 +0800

    fix: ci lint

--- core/relay/adaptor/anthropic/render.go
+++ core/relay/adaptor/anthropic/render.go
@@ -19,7 +19,18 @@ func Data(c *gin.Context, data []byte) {
 	c.Writer.Flush()
 }
 
-func ObjectData(c *gin.Context, event string, object any) error {
+func EventData(c *gin.Context, event string, data []byte) {
+	if len(c.Errors) > 0 {
+		return
+	}
+	if c.IsAborted() {
+		return
+	}
+	c.Render(-1, &Anthropic{Event: event, Data: data})
+	c.Writer.Flush()
+}
+
+func ObjectData(c *gin.Context, object any) error {
 	if len(c.Errors) > 0 {
 		return c.Errors.Last()
 	}
@@ -31,6 +42,22 @@ func ObjectData(c *gin.Context, event string, object any) error {
 		return fmt.Errorf("error marshalling object: %w", err)
 	}
 	c.Render(-1, &Anthropic{Data: jsonData})
+	c.Writer.Flush()
+	return nil
+}
+
+func EventObjectData(c *gin.Context, event string, object any) error {
+	if len(c.Errors) > 0 {
+		return c.Errors.Last()
+	}
+	if c.IsAborted() {
+		return errors.New("context aborted")
+	}
+	jsonData, err := sonic.Marshal(object)
+	if err != nil {
+		return fmt.Errorf("error marshalling object: %w", err)
+	}
+	c.Render(-1, &Anthropic{Event: event, Data: jsonData})
 	c.Writer.Flush()
 	return nil
 }
