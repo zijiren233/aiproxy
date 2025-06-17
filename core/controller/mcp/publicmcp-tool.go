542d6996575cd0ffdd1221951b53d892e20fc868
zijiren233 <pyh1670605849@gmail.com>
2025-06-17 16:30:42 +0800

    fix: clean tool cache expited

--- core/controller/mcp/publicmcp-tool.go
+++ core/controller/mcp/publicmcp-tool.go
@@ -40,6 +40,7 @@ func (r redisToolSlice) MarshalBinary() ([]byte, error) {
 type toolCacheMemItem struct {
 	tools      []mcp.Tool
 	lastUsedAt time.Time
+	expiresAt  time.Time
 }
 
 type toolMemoryCache struct {
@@ -60,9 +61,11 @@ func (c *toolMemoryCache) set(key string, tools []mcp.Tool) {
 	c.startCleanupOnStart()
 	c.mu.Lock()
 	defer c.mu.Unlock()
+	now := time.Now()
 	c.items[key] = toolCacheMemItem{
 		tools:      tools,
-		lastUsedAt: time.Now(),
+		lastUsedAt: now,
+		expiresAt:  now.Add(time.Hour),
 	}
 }
 
@@ -74,13 +77,14 @@ func (c *toolMemoryCache) get(key string) ([]mcp.Tool, bool) {
 	if !exists {
 		return nil, false
 	}
-
-	if time.Now().After(item.lastUsedAt.Add(time.Minute * 10)) {
+	now := time.Now()
+	if now.After(item.lastUsedAt.Add(time.Minute*10)) ||
+		now.After(item.expiresAt) {
 		delete(c.items, key)
 		return nil, false
 	}
 
-	item.lastUsedAt = time.Now()
+	item.lastUsedAt = now
 
 	return item.tools, true
 }
@@ -91,7 +95,8 @@ func (c *toolMemoryCache) cleanup() {
 
 	now := time.Now()
 	for key, item := range c.items {
-		if now.After(item.lastUsedAt.Add(time.Minute * 10)) {
+		if now.After(item.lastUsedAt.Add(time.Minute*10)) ||
+			now.After(item.expiresAt) {
 			delete(c.items, key)
 		}
 	}
